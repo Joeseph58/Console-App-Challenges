7435bd82d5b843ddd951c1ed47fdc521a8bb59f4
Joeseph58 <Joeseph58@users.noreply.github.com>
2019-09-30 14:00:44 -0400

    fourth push

--- 01_Challenge_Test/UnitTest1.cs
+++ 01_Challenge_Test/UnitTest1.cs
@@ -5,14 +5,49 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace _01_Challenge_Test
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTest
     {
-        [Testmethod]
-        public void Testmethod1()
+        [TestMethod]
+        public void Method()
         {
             //List<Menu> = new <Menu>List()
+            Menu_Repository _menuList = new Menu_Repository();
+            _menuList.SeedList();
 
-          
+            Assert.AreEqual(3, _menuList.GetMenu().Count);
+
         }
+
+        [TestMethod]
+        public void AddMethod()
+        {
+            Menu_Repository _menuList = new Menu_Repository();
+            Menu content = new Menu();
+
+            _menuList.AddToMenu(content);
+
+
+            Assert.AreEqual(1, _menuList.GetMenu().Count);
+
+        }
+        [TestMethod]
+        public void RemoveMethod()
+        {
+            Menu_Repository _menuList = new Menu_Repository();
+            _menuList.SeedList();
+
+            _menuList.RemoveFromMenu(1);
+            Assert.AreEqual(_menuList.GetMenu().Count, 2);
+
+
+        }
+        [TestMethod]
+        public void CreateMethod()
+        {
+            Menu_Repository _menuList = new Menu_Repository();
+            _menuList.
+
+        }
+
     }
 }
